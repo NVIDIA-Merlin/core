e8c15a9edcbd08610a806de4220a60a7c8535be9
Karl Higley <kmhigley@gmail.com>
2022-02-03 14:12:44 -0500

    Add graph package

---
+++ tests/conftest.py
@@ -0,0 +1,239 @@
+#
+# Copyright (c) 2022, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import contextlib
+import glob
+import os
+import random
+
+import dask
+import numpy as np
+import pandas as pd
+
+try:
+    import cudf
+
+    try:
+        import cudf.testing._utils
+
+        assert_eq = cudf.testing._utils.assert_eq
+    except ImportError:
+        import cudf.tests.utils
+
+        assert_eq = cudf.tests.utils.assert_eq
+except ImportError:
+    cudf = None
+
+    def assert_eq(a, b, *args, **kwargs):
+        if isinstance(a, pd.DataFrame):
+            return pd.testing.assert_frame_equal(a, b, *args, **kwargs)
+        elif isinstance(a, pd.Series):
+            return pd.testing.assert_series_equal(a, b, *args, **kwargs)
+        else:
+            return np.testing.assert_allclose(a, b)
+
+
+import pytest
+from dask.distributed import Client, LocalCluster
+from numba import cuda
+
+import merlin.graph
+
+allcols_csv = ["timestamp", "id", "label", "name-string", "x", "y", "z"]
+mycols_csv = ["name-string", "id", "label", "x", "y"]
+mycols_pq = ["name-cat", "name-string", "id", "label", "x", "y"]
+mynames = [
+    "Alice",
+    "Bob",
+    "Charlie",
+    "Dan",
+    "Edith",
+    "Frank",
+    "Gary",
+    "Hannah",
+    "Ingrid",
+    "Jerry",
+    "Kevin",
+    "Laura",
+    "Michael",
+    "Norbert",
+    "Oliver",
+    "Patricia",
+    "Quinn",
+    "Ray",
+    "Sarah",
+    "Tim",
+    "Ursula",
+    "Victor",
+    "Wendy",
+    "Xavier",
+    "Yvonne",
+    "Zelda",
+]
+
+_CUDA_CLUSTER = None
+
+
+@pytest.fixture(scope="module")
+def client():
+    cluster = LocalCluster(n_workers=2)
+    client = Client(cluster)
+    yield client
+    client.close()
+    cluster.close()
+
+
+@contextlib.contextmanager
+def get_cuda_cluster():
+    from dask_cuda import LocalCUDACluster
+
+    CUDA_VISIBLE_DEVICES = os.environ.get("CUDA_VISIBLE_DEVICES", "0")
+    n_workers = min(2, len(CUDA_VISIBLE_DEVICES.split(",")))
+    cluster = LocalCUDACluster(n_workers=n_workers)
+    yield cluster
+    cluster.close()
+
+
+@pytest.fixture(scope="session")
+def datasets(tmpdir_factory):
+    _lib = cudf if cudf else pd
+    _datalib = cudf if cudf else dask
+    df = _datalib.datasets.timeseries(
+        start="2000-01-01",
+        end="2000-01-04",
+        freq="60s",
+        dtypes={
+            "name-cat": str,
+            "name-string": str,
+            "id": int,
+            "label": int,
+            "x": float,
+            "y": float,
+            "z": float,
+        },
+    ).reset_index()
+
+    if _datalib is dask:
+        df = df.compute()
+
+    df["name-string"] = _lib.Series(np.random.choice(mynames, df.shape[0])).astype("O")
+
+    # Add two random null values to each column
+    imax = len(df) - 1
+    for col in df.columns:
+        if col in ["name-cat", "label", "id"]:
+            break
+        for _ in range(2):
+            rand_idx = random.randint(1, imax - 1)
+            if rand_idx == df[col].shape[0] // 2:
+                # dont want null in median
+                rand_idx += 1
+            df[col].iloc[rand_idx] = None
+
+    datadir = tmpdir_factory.mktemp("data_test")
+    datadir = {
+        "parquet": tmpdir_factory.mktemp("parquet"),
+        "csv": tmpdir_factory.mktemp("csv"),
+        "csv-no-header": tmpdir_factory.mktemp("csv-no-header"),
+        "cats": tmpdir_factory.mktemp("cats"),
+    }
+
+    half = int(len(df) // 2)
+
+    # Write Parquet Dataset
+    df.iloc[:half].to_parquet(str(datadir["parquet"].join("dataset-0.parquet")), chunk_size=1000)
+    df.iloc[half:].to_parquet(str(datadir["parquet"].join("dataset-1.parquet")), chunk_size=1000)
+
+    # Write CSV Dataset (Leave out categorical column)
+    df.iloc[:half].drop(columns=["name-cat"]).to_csv(
+        str(datadir["csv"].join("dataset-0.csv")), index=False
+    )
+    df.iloc[half:].drop(columns=["name-cat"]).to_csv(
+        str(datadir["csv"].join("dataset-1.csv")), index=False
+    )
+    df.iloc[:half].drop(columns=["name-cat"]).to_csv(
+        str(datadir["csv-no-header"].join("dataset-0.csv")), header=False, index=False
+    )
+    df.iloc[half:].drop(columns=["name-cat"]).to_csv(
+        str(datadir["csv-no-header"].join("dataset-1.csv")), header=False, index=False
+    )
+
+    return datadir
+
+
+@pytest.fixture(scope="function")
+def paths(engine, datasets):
+    return sorted(glob.glob(str(datasets[engine]) + "/*." + engine.split("-")[0]))
+
+
+@pytest.fixture(scope="function")
+def df(engine, paths):
+    _lib = cudf if cudf else pd
+    if engine == "parquet":
+        df1 = _lib.read_parquet(paths[0])[mycols_pq]
+        df2 = _lib.read_parquet(paths[1])[mycols_pq]
+    elif engine == "csv-no-header":
+        df1 = _lib.read_csv(paths[0], header=None, names=allcols_csv)[mycols_csv]
+        df2 = _lib.read_csv(paths[1], header=None, names=allcols_csv)[mycols_csv]
+    elif engine == "csv":
+        df1 = _lib.read_csv(paths[0], header=0)[mycols_csv]
+        df2 = _lib.read_csv(paths[1], header=0)[mycols_csv]
+    else:
+        raise ValueError("unknown engine:" + engine)
+
+    gdf = _lib.concat([df1, df2], axis=0)
+    gdf["id"] = gdf["id"].astype("int64")
+    return gdf
+
+
+@pytest.fixture(scope="function")
+def dataset(request, paths, engine):
+    try:
+        gpu_memory_frac = request.getfixturevalue("gpu_memory_frac")
+    except Exception:  # pylint: disable=broad-except
+        gpu_memory_frac = 0.01
+
+    try:
+        cpu = request.getfixturevalue("cpu")
+    except Exception:  # pylint: disable=broad-except
+        cpu = False
+
+    kwargs = {}
+    if engine == "csv-no-header":
+        kwargs["names"] = allcols_csv
+
+    return merlin.Dataset(paths, part_mem_fraction=gpu_memory_frac, cpu=cpu, **kwargs)
+
+
+def run_in_context(func, *args, context=None, **kwargs):
+    # Convenience utility to execute a function within
+    # a specific `context`.  For example, this can be
+    # used to test that a function raises a `UserWarning`
+    # by setting `context=pytest.warns(UserWarning)`
+    if context is None:
+        context = contextlib.suppress()
+    with context:
+        result = func(*args, **kwargs)
+    return result
+
+
+# Allow to pass devices as parameters
+def pytest_addoption(parser):
+    parser.addoption("--devices", action="store", default="0", help="0,1,..,n-1")
+
+
+@pytest.fixture
+def devices(request):
+    return request.config.getoption("--devices")
---
+++ tests/unit/graph/test_base_operator.py
@@ -0,0 +1,78 @@
+#
+# Copyright (c) 2021, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import pytest
+
+from merlin.graph.base_operator import BaseOperator as Operator
+from merlin.graph.graph import Graph
+from merlin.graph.schema import Schema
+from merlin.graph.selector import ColumnSelector
+
+
+@pytest.mark.parametrize("engine", ["parquet"])
+def test_graph_validates_schemas(dataset, engine):
+    ops = ["a", "b", "c"] >> Operator()
+    graph = Graph(ops)
+
+    with pytest.raises(ValueError) as exc_info:
+        graph.construct_schema(dataset.schema)
+
+    assert "Missing column" in str(exc_info.value)
+
+
+@pytest.mark.parametrize("engine", ["parquet"])
+def test_compute_selector_validates_schemas(dataset, engine):
+    op = BaseOperator()
+    schema = Schema(["a", "b"])
+    selector = ColumnSelector(["c"])
+
+    with pytest.raises(ValueError) as exc_info:
+        op.compute_selector(schema, selector, ColumnSelector(), ColumnSelector())
+
+    assert "Missing column" in str(exc_info.value)
+
+
+@pytest.mark.parametrize("engine", ["parquet"])
+def test_compute_input_schema_validates_schemas(dataset, engine):
+    op = Operator()
+    schema = Schema(["a", "b"])
+    selector = ColumnSelector(["c"])
+
+    with pytest.raises(ValueError) as exc_info:
+        op.compute_input_schema(schema, Schema(), Schema(), selector)
+
+    assert "Missing column" in str(exc_info.value)
+
+    with pytest.raises(ValueError) as exc_info:
+        op.compute_input_schema(Schema(), schema, Schema(), selector)
+
+    assert "Missing column" in str(exc_info.value)
+
+    with pytest.raises(ValueError) as exc_info:
+        op.compute_input_schema(Schema(), Schema(), schema, selector)
+
+    assert "Missing column" in str(exc_info.value)
+
+
+@pytest.mark.parametrize("engine", ["parquet"])
+def test_compute_output_schema_validates_schemas(dataset, engine):
+    op = Operator()
+    schema = Schema(["a", "b"])
+    selector = ColumnSelector(["c"])
+
+    with pytest.raises(ValueError) as exc_info:
+        op.compute_output_schema(schema, selector)
+
+    assert "Missing column" in str(exc_info.value)
