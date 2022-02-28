975b67e82847f139bf3e4cd5628375e0fa02d348
Karl Higley <kmhigley@gmail.com>
2022-02-28 13:22:15 -0500

    De-duplicate nodes in queue-based graph iteration methods

--- merlin/dag/node.py
+++ merlin/dag/node.py
@@ -427,10 +427,9 @@ def iter_nodes(nodes):
             queue.extend(current)
         else:
             yield current
-            # TODO: deduplicate nodes?
             for node in current.parents_with_dependencies:
-
-                queue.append(node)
+                if node not in queue:
+                    queue.append(node)
 
 
 # output node (bottom) -> selection leaf nodes (top)
@@ -441,7 +440,13 @@ def preorder_iter_nodes(nodes):
 
     def traverse(current_nodes):
         for node in current_nodes:
+            # Avoid creating duplicate nodes in the queue
+            if node in queue:
+                queue.remove(node)
+
             queue.append(node)
+
+        for node in current_nodes:
             traverse(node.parents_with_dependencies)
 
     traverse(nodes)
@@ -458,7 +463,8 @@ def postorder_iter_nodes(nodes):
     def traverse(current_nodes):
         for node in current_nodes:
             traverse(node.parents_with_dependencies)
-            queue.append(node)
+            if node not in queue:
+                queue.append(node)
 
     traverse(nodes)
     for node in queue:
