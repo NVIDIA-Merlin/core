c405f4719782ee057b29ef2b069731fffb096a50
Karl Higley <kmhigley@gmail.com>
2022-10-20 10:17:04 -0400

    Patch `is_list_dtype`/`list_val_dtype` to work with Numpy ndarrays (#153)

--- merlin/core/dispatch.py
+++ merlin/core/dispatch.py
@@ -292,6 +292,8 @@ def list_val_dtype(ser: SeriesLike) -> np.dtype:
             return ser.dtype
         elif isinstance(ser, pd.Series):
             return pd.core.dtypes.cast.infer_dtype_from(next(iter(pd.core.common.flatten(ser))))[0]
+    if isinstance(ser, np.ndarray):
+        return ser.dtype
     return None
 
 
@@ -305,7 +307,7 @@ def is_list_dtype(ser):
         # either np.ndarray or a dtype
         if isinstance(ser, np.ndarray):
             ser = ser[0]
-        return pd.api.types.is_list_like(np.dtype(ser))
+        return pd.api.types.is_list_like(ser)
     return cudf_is_list_dtype(ser)
 
 
