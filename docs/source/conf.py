f7379354c06f0037c282bec00e1b5757bcd114dc
Mike McKiernan <mmckiernan@nvidia.com>
2022-11-08 07:23:39 -0500

    docs: Add temp semver to calver banner
    In preparation of switching from semantic versioning, like
    `0.4.0`, to calendar versioning, like `23.01`, temporarily
    add a banner to all the documentation pages.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,6 +89,7 @@ html_show_sourcelink = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
 
 if os.path.exists(gitdir):
     tag_refs = subprocess.check_output(["git", "tag", "-l", "v*"]).decode("utf-8").split()
