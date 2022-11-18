6610d7f31c395fe2e223ea6748ff1432066f3c14
Oliver Holworthy <oholworthy@nvidia.com>
2022-11-18 11:47:48 +0000

    Update formatting.
    Would be nice if the pre-commit versions matched the versions of things in CI

--- tests/unit/schema/test_schema_io.py
+++ tests/unit/schema/test_schema_io.py
@@ -81,7 +81,7 @@ def test_merlin_to_proto_to_json_to_merlin():
         [{"min": 0, "max": 0}, False, False],
         [{"min": 1, "max": 1}, True, False],
         [{"min": 1, "max": 2}, True, True],
-    ]
+    ],
 )
 def test_value_count(value_count, expected_is_list, expected_is_ragged):
     schema = Schema(
