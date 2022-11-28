7686daa74222f57854a5bfe231d8d7a6ef4bc788
Karl Higley <kmhigley@gmail.com>
2022-11-28 12:36:46 -0500

    Remove stray `tritonclient` import

--- merlin/dtype/mappings.py
+++ merlin/dtype/mappings.py
@@ -16,10 +16,8 @@
 
 import numpy as np
 
-from merlin.core.compat import tritonclient
 from merlin.dtype import dtypes
 from merlin.dtype.registry import _dtype_registry
-
 
 python_dtypes = {
     dtypes.boolean: bool,
@@ -36,18 +34,15 @@ numpy_dtypes = {
     dtypes.uint16: [np.dtype("uint16"), np.uint16],
     dtypes.uint32: [np.dtype("uint32"), np.uint32],
     dtypes.uint64: [np.dtype("uint64"), np.uint64],
-
     # Signed integer
     dtypes.int8: [np.dtype("int8"), np.int8],
     dtypes.int16: [np.dtype("int16"), np.int16],
     dtypes.int32: [np.dtype("int32"), np.int32],
     dtypes.int64: [np.dtype("int64"), np.int64],
-    
     # Floating Point
     dtypes.float16: [np.dtype("float16"), np.float16],
     dtypes.float32: [np.dtype("float32"), np.float32],
     dtypes.float64: [np.dtype("float64"), np.float64],
-    
     # Date/Time
     dtypes.datetime64: [np.dtype("datetime64"), np.datetime64],
     dtypes.datetime64Y: [np.dtype("datetime64[Y]")],
@@ -59,7 +54,6 @@ numpy_dtypes = {
     dtypes.datetime64ms: [np.dtype("datetime64[ms]")],
     dtypes.datetime64us: [np.dtype("datetime64[us]")],
     dtypes.datetime64ns: [np.dtype("datetime64[ns]")],
-    
     # Miscellaneous
     dtypes.string: [np.dtype("str"), np.str],
     dtypes.object_: [np.dtype("O")],
@@ -78,18 +72,17 @@ try:
         dtypes.uint16: [model_config.TYPE_UINT16],
         dtypes.uint32: [model_config.TYPE_UINT32],
         dtypes.uint64: [model_config.TYPE_UINT64],
-
         # Signed integer
         dtypes.int8: [model_config.TYPE_INT8],
         dtypes.int16: [model_config.TYPE_INT16],
         dtypes.int32: [model_config.TYPE_INT32],
         dtypes.int64: [model_config.TYPE_INT64],
-        
         # Floating Point
         dtypes.float16: [model_config.TYPE_FP16],
-        dtypes.float32: [model_config.TYPE_FP32,],
+        dtypes.float32: [
+            model_config.TYPE_FP32,
+        ],
         dtypes.float64: [model_config.TYPE_FP64],
-        
         # Miscellaneous
         dtypes.string: [model_config.TYPE_STRING],
         dtypes.boolean: [model_config.TYPE_BOOL],
