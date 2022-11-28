81b6252a34623314204ee32fd8d2d9a85cca96da
Karl Higley <kmhigley@gmail.com>
2022-11-28 16:49:18 -0500

    Add matching and translation methods to `DTypeMapping`

--- merlin/dtype/registry.py
+++ merlin/dtype/registry.py
@@ -13,19 +13,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import dataclasses
 from typing import Dict, Union
 
 
 class DTypeMapping:
-    def __init__(self, mapping):
-        self.from_merlin = mapping
-        self.to_merlin = {}
+    def __init__(self, mapping, base_class=None):
+        self.from_merlin_ = mapping
+        self.to_merlin_ = {}
+        self.base_class = base_class
 
         for key, values in mapping.items():
             if not isinstance(values, list):
                 values = [values]
             for value in values:
-                self.to_merlin[value] = key
+                self.to_merlin_[value] = key
+
+    def matches_external(self, dtype):
+        return self._matches(dtype, self.to_merlin_, self.base_class)
+
+    def matches_merlin(self, dtype):
+        return self._matches(dtype, self.from_merlin_)
+
+    def to_merlin(self, external_dtype, shape = None):
+        merlin_dtype = self.to_merlin_[external_dtype]
+        return dataclasses.replace(merlin_dtype, **{"shape": shape})
+
+    def from_merlin(self, merlin_dtype):
+        # Ignore the shape when matching dtypes
+        shapeless_merlin_dtype = dataclasses.replace(merlin_dtype, **{"shape": None})
+        # Always translate to the first external dtype in the list
+        return self.from_merlin_[shapeless_merlin_dtype][0]
+
+    def _matches(self, dtype, mapping, base_class = None):
+        if base_class and not isinstance(dtype, base_class):
+            return False
+
+        return dtype in mapping.keys()
 
 
 class DTypeMappingRegistry:
