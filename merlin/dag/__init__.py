b23b0426285f8ff1cb64fdb86d1eb51469bd10e1
Karl Higley <kmhigley@gmail.com>
2023-04-12 17:21:19 -0400

    Remove `DictArray`

--- merlin/dag/__init__.py
+++ merlin/dag/__init__.py
@@ -16,7 +16,6 @@
 
 # flake8: noqa
 from merlin.dag.base_operator import BaseOperator, DataFormats, Supports
-from merlin.dag.dictarray import DictArray
 from merlin.dag.graph import Graph
 from merlin.dag.node import Node, iter_nodes, postorder_iter_nodes, preorder_iter_nodes
 from merlin.dag.selector import ColumnSelector
