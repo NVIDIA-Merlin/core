d7b42154c6ce4bf2a2828817d9b17c34a5cf54a7
Karl Higley <kmhigley@gmail.com>
2023-05-12 16:06:29 -0400

    Add a utility for testing equality between `TensorTables`

---
+++ merlin/testing/assert_equals.py
@@ -0,0 +1,58 @@
+#
+# Copyright (c) 2023, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from merlin.core.compat import pandas as pd
+from merlin.dispatch.lazy import lazy_singledispatch
+from merlin.table import TensorTable
+
+
+def assert_table_equal(left: TensorTable, right: TensorTable):
+    pd.testing.assert_frame_equal(left.cpu().to_df(), right.cpu().to_df())
+
+
+# @lazy_singledispatch
+# def assert_column_equal(left, right):
+#     raise NotImplementedError
+
+
+@lazy_singledispatch
+def assert_transformable_equal(left, right):
+    raise NotImplementedError
+
+
+@assert_transformable_equal.register_lazy("cudf")
+def _register_assert_equal_df_cudf():
+    import cudf
+
+    @assert_transformable_equal.register(cudf.DataFrame)
+    def _assert_equal_df_cudf(left, right):
+        return cudf.testing.assert_frame_equal(left, right)
+
+
+@assert_transformable_equal.register_lazy("pandas")
+def _register_assert_equal_pandas():
+    import pandas
+
+    @assert_transformable_equal.register(pandas.DataFrame)
+    def _assert_equal_pandas(left, right):
+        return pandas.testing.assert_frame_equal(left, right)
+
+
+@assert_transformable_equal.register_lazy("merlin")
+def _register_assert_equal_merlin():
+    @assert_transformable_equal.register(TensorTable)
+    def _assert_equal_table(left, right):
+        return assert_table_equal(left, right)
