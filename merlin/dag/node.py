38fdb24e768fdfba3d64ea349ac61bb438cbe125
Julio <jperez@nvidia.com>
2023-06-13 21:13:01 -0400

    add subgraph logic to post and pre order traversal

--- merlin/dag/node.py
+++ merlin/dag/node.py
@@ -624,6 +624,8 @@ def preorder_iter_nodes(nodes):
             if node in queue:
                 queue.remove(node)
 
+            if node.op.is_subgraph:
+                queue.extend(list(preorder_iter_nodes(node.op.graph.output_node)))
             queue.append(node)
 
         for node in current_nodes:
@@ -643,8 +645,11 @@ def postorder_iter_nodes(nodes):
     def traverse(current_nodes):
         for node in current_nodes:
             traverse(node.parents_with_dependencies)
+
             if node not in queue:
                 queue.append(node)
+            if node.op.is_subgraph:
+                queue.extend(list(postorder_iter_nodes(node.op.graph.output_node)))
 
     traverse(nodes)
     for node in queue:
