763663a80cfc680512100ebaf4e2d5abfc7f8c8d
Julio <jperez@nvidia.com>
2023-07-06 11:59:41 -0400

    move base runtime to core

--- merlin/dag/__init__.py
+++ merlin/dag/__init__.py
@@ -16,6 +16,7 @@
 
 # flake8: noqa
 from merlin.dag.base_operator import BaseOperator, DataFormats, Supports
+from merlin.dag.base_runtime import Runtime
 from merlin.dag.graph import Graph
 from merlin.dag.node import Node, iter_nodes, postorder_iter_nodes, preorder_iter_nodes
 from merlin.dag.selector import ColumnSelector
