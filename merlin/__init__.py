884dc26ca9c3814d61a842065c6ac89fa5c86328
rjzamora <rzamora217@gmail.com>
2024-07-17 07:19:17 -0700

    move dask config settings/check to top level (only way to be sure we don't import dd anywere before disabling these settings)

---
+++ merlin/__init__.py
@@ -0,0 +1,48 @@
+#
+# Copyright (c) 2024, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+_DASK_QUERY_PLANNING_ENABLED = False
+try:
+    # Disable query-planning and string conversion
+    import dask
+
+    dask.config.set(
+        {
+            "dataframe.query-planning": False,
+            "dataframe.convert-string": False,
+        }
+    )
+except ImportError:
+    pass
+else:
+    import sys
+
+    import dask.dataframe as dd
+    from packaging.version import parse
+
+    if parse(dask.__version__) > parse("2024.6.0"):
+        _DASK_QUERY_PLANNING_ENABLED = dd.DASK_EXPR_ENABLED
+    else:
+        _DASK_QUERY_PLANNING_ENABLED = "dask_expr" in sys.modules
+
+
+if _DASK_QUERY_PLANNING_ENABLED:
+    raise NotImplementedError(
+        "Merlin does not support the query-planning API in Dask "
+        "Dataframe yet. Please make sure query-planning is "
+        "disabled before dask.dataframe is imported. E.g.:\n"
+        "dask.config.set({'dataframe.query-planning': False})"
+    )
