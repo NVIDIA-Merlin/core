4eb42dbf9a038e5ba86ff36c7e824e54b547b7c9
rjzamora <rzamora217@gmail.com>
2024-07-19 10:08:04 -0700

    add deprecation message

--- merlin/io/dataset_engine.py
+++ merlin/io/dataset_engine.py
@@ -52,7 +52,12 @@ class DatasetEngine:
         raise NotImplementedError(""" Returns the number of rows in the dataset """)
 
     def validate_dataset(self, **kwargs):
-        raise NotImplementedError(""" Returns True if the raw data is efficient for NVTabular """)
+        raise NotImplementedError(
+            """The `validate_dataset` method is no longer supported
+            for merlin versions >23.08, because it relies on deprecated
+            functionality in pyarrow.
+            """
+        )
 
     @classmethod
     def regenerate_dataset(cls, dataset, output_path, columns=None, **kwargs):
